84fd6c47f3da581abd72c67480f262300aecbca5
Hugo Jahnke <hugo.jahnke@gmail.com>
2020-03-04 22:15:31 +0100

    Store Release Ajout propriété artists Implémentation mutation, action, getter

--- src/store/modules/release.js
+++ src/store/modules/release.js
@@ -1,33 +1,55 @@
 import axios from 'axios'
 
 const state = {
+    artists: [],
     tracklist: []
 }
 
 const getters = {
-    tracklist: state => { return state.tracklist }
+    tracklist: state => { return state.tracklist },
+    artists: state => { return state.artists }
 }
 
 const mutations = {
     setTracklist: function(state, tracklist){
       state.tracklist = tracklist
     },
+    setArtists: function(state, artists){
+      state.artists = artists
+    },
     reset: function (state) {
+        state.artists = []
         state.tracklist = []
     }
 }
 
 const actions = {
     getTracklist: function ({commit}, id) {
-        const url = `http://musicbrainz.org/ws/2/release/${id}?inc=recordings&fmt=json`
+        const url = `http://musicbrainz.org/ws/2/recording?release=${id}&fmt=json`
         return new Promise((resolve, reject) => {
             axios.get(url)
                 .then(res => {
-                    commit('setTracklist', res.data.media[0].tracks)
+                    commit('setTracklist', res.data.recordings)
                     resolve()
                 })
                 .catch(err => reject(err))
         })
+    },
+    getArtists: function ({commit}, id) {
+        const url = `http://musicbrainz.org/ws/2/artist?release=${id}&fmt=json`
+        return new Promise((resolve, reject) => {
+            axios.get(url)
+                .then(res => {
+                    commit('setArtists', res.data.artists)
+                    resolve()
+                })
+                .catch(err => reject(err))
+        })
+    },
+    getData: function ({dispatch, commit}, id) {
+        dispatch('getTracklist', id)
+            .then(() => { return dispatch('getArtists', id) })
+            .catch(err => commit('setError', err, {root: true}))
     }
 }
 
