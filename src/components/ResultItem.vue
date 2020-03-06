436286d7c85ef52af1902916b12424daa406da2f
Hugo Jahnke <hugo.jahnke@gmail.com>
2020-03-06 11:35:49 +0100

    Mobile - Style ResultItem

--- src/components/ResultItem.vue
+++ src/components/ResultItem.vue
@@ -1,21 +1,41 @@
 <template>
-    <div class="result-item">
-        <h4>{{ name }}</h4>
-        <button @click="goToDetails">details</button>
-    </div>
+    <li class="result-item">
+        <h4 @click="goToDetails">{{ name }}</h4>
+        <ul class="info">
+            <li v-if="type === 'artist'"><i class="fas fa-info-circle"></i>{{ about }}</li>
+            <li v-if="type === 'artist'"><i class="fas fa-globe"></i>{{ country }}</li>
+            <li v-if="type === 'release' || type === 'recording'"><i class="fas fa-users"></i>{{ artist }}</li>
+            <li v-if="type === 'recording'"><i class="fas fa-compact-disc"></i>{{ release }}</li>
+        </ul>
+    </li>
 </template>
 
 <script>
-    import {mapMutations} from 'vuex'
+    import {mapMutations, mapGetters} from 'vuex'
     export default {
         name: "ResultItem",
         props: ['result'],
         computed: {
+            ...mapGetters({
+                type: 'search/selectedType'
+            }),
             id: function () {
                 return this.result.id
             },
             name: function () {
                 return this.result.name || this.result.title
+            },
+            about: function () {
+                return this.result.disambiguation || 'no informations'
+            },
+            country: function () {
+                return this.result.country || 'unknown'
+            },
+            artist: function () {
+                return this.result['artist-credit'][0].name || 'unknown'
+            },
+            release: function () {
+                return this.result.releases[0].title || 'unknown'
             }
         },
         methods: {
@@ -28,6 +48,22 @@
     }
 </script>
 
-<style scoped>
-
+<style lang="sass" scoped>
+    @import '../sass/scheme'
+    .result-item
+        background-color: $lightgrey
+        color: $blue
+        padding: 4px
+        h4
+            width: fit-content
+            &:hover
+                cursor: pointer
+                text-decoration: underline
+        .info
+            padding: 0
+            overflow: scroll
+            li
+                list-style: none
+                svg
+                    margin-right: 5px
 </style>
