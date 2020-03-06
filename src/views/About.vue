a8bc7f0685b095fe8459aaa2975bc6b85f033a71
Hugo Jahnke <hugo.jahnke@gmail.com>
2020-03-06 18:41:21 +0100

    Redaction section About

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,10 +1,28 @@
 <template>
-    <div class="about">
+    <div id="about">
         <section>
             <h3>the project</h3>
+            <p>
+                <b>musearch</b> is an homework project about creating an application using Vue.js
+            </p>
+            <p>
+                the app also uses <a href="https://vuex.vuejs.org/">Vuex</a> for the state management and
+                <a href="https://github.com/axios/axios">axios</a> for the API calls
+            </p>
+            <p>
+                the whole code is available on <a href="https://github.com/Nekika/musearch.git">GitHub</a>
+            </p>
         </section>
         <section>
             <h3>the author</h3>
+            <p>
+                my name is <b>Hugo</b> and at the time of writing this i'm a french web development student
+                from Nancy
+            </p>
+            <div class="icons">
+                <a href="https://github.com/Nekika" target="_blank"><i class="fab fa-github-square"></i></a>
+                <a href="https://linkedin.com/in/hugo-jahnke-a96ba8146" target="_blank"><i class="fab fa-linkedin"></i></a>
+            </div>
         </section>
     </div>
 </template>
@@ -16,5 +34,19 @@
 </script>
 
 <style lang="sass" scoped>
+    @import '../sass/scheme'
+
+    #about
+        color: $blue
+        margin: 20px 10px 0 10px
+        .icons
+            font-size: 30pt
+            text-align: center
+            a
+                color: $blue
+                &:hover
+                    color: $lightgrey
+                svg
+                    margin: 8px
 
 </style>
